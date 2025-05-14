988a9152297894aeb62cff155808c8d1c7acd737
tangzhouxuan-nju <820097351@qq.com>
2025-05-14 21:14:11 +0800

    path2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{fs::File, io::Read};
 
 fn main() -> std::io::Result<()> {
-    let path1="path/to/file.txt";
-    let mut file = File::open(path1)?; 
+    let path2="path/to/file.txt";
+    let mut file = File::open(path2)?; 
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;  
     println!("contents: {}", contents);
