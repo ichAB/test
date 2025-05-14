ecbdcd81d346f9ae7bc9aff7b813684b39a63918
tangzhouxuan-nju <820097351@qq.com>
2025-05-14 21:13:52 +0800

    path1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::Read};
+
+fn main() -> std::io::Result<()> {
+    let path1="path/to/file.txt";
+    let mut file = File::open(path1)?; 
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;  
+    println!("contents: {}", contents);
+    Ok(())
 }
